0d16932b3eae5d1675a335256a5b9e52090bd96a
Taletex <alessandromessina95@hotmail.com>
2019-03-01 17:08:12 +0100

    Updated R client

--- DiagnosticClient/Main.r
+++
@@ -1,8 +0,0 @@
-a <- 42
-A <- a * 2  # R is case sensitive
-print(a)
-cat(A, "\n") # "84" is concatenated with "\n"
-if(A>a) # true, 84 > 42
-{
-  cat(A, ">", a, "\n")
-} 
--- DiagnosticClient/app/app.R
+++ DiagnosticClient/app/app.R
@@ -1,5 +1,6 @@
-library(shiny)
-require(graphics) 
+library(shiny)    
+library(ggplot2)
+library(jsonlite)
 
 # Define UI ----
 ui <- fluidPage(
@@ -35,9 +36,7 @@ ui <- fluidPage(
       h2("Results"),
       textOutput("title"),
       br(),
-      plotOutput("blocks"),
-      plotOutput("transactions"),
-      plotOutput("coins")
+      plotOutput("statgraph")
     )
   )
 )
@@ -51,27 +50,25 @@ server <- function(input, output) {
   #load(file="/home/alessandro/Projects/SkanCoin/DiagnosticClient/app/data/data.Rda")     now "stat" variable contains the saved dataframe
   
   
-  # OBSERVING VARIABLES #
-  v <- reactiveValues(data = "")
+  # OBSERVING VARIABLES AND EVENTS #
+  v <- reactiveValues(query = 0, title = "", data = "")
   
   observeEvent(input$query1, {
-    load(file="/home/alessandro/Projects/SkanCoin/DiagnosticClient/app/data/data.Rda")
-    
     v$query <- 1
-    v$title <- "Statistiche temporali della blockchain"
-    v$data <- stat
+    v$title <- "Statistiche temporali della blockchain (numero di blocchi, numero di transazioni e numero di coin)"
+    v$data <- getStats()
   })
   
   observeEvent(input$query2, {
     v$query <- 2
     v$title <- "Tempo di mining di ogni blocco"
-    v$data <- ""
+    v$data <- getBlocksMiningTime()
   })
   
   observeEvent(input$query3, {
     v$query <- 3
     v$title <- "Tempo di attesa per la conferma di ogni transazione"
-    v$data <- ""
+    v$data <- getWaitingTransactionMiningTime()
   })
   
   
@@ -80,30 +77,76 @@ server <- function(input, output) {
     v$title
   })
   
-  output$blocks <- renderPlot({
-    x <- ts(v$data["time"])
-    y <- ts(v$data["blocks"])
-    plot.ts(x, y, main = v$title, xlab = "Time", ylab = "Value")
-    points(x,y)
-    #legend("top", legend = "Lunghezza della Blockchain", col = "black", lty = 1)
+  output$statgraph <- renderPlot({
+    # Commenti da rimuovere
+    #legend(1, 50, legend = c("Blocchi", "Transazioni", "Coins"), col = c("blue", "red", "black"), pch=c("o", "*", "+"), lty = c(1, 2, 3), ncol=2)
+    #x <- ts(v$data["time"])
+    #y1 <- ts(v$data["blocks"])
+    #y2 <- ts(v$data["transactions"])
+    #y3 <- ts(v$data["coins"])
+    #plot(x, y1, main="Blocchi", xlab = "Time", type="o", col="blue", pch="o", lty=1)
+    #points(x, y2, col="red", pch="*")
+    #lines(x, y2, col="red", lty=2)
+    #points(x, y3, col="dark red", pch="+")
+    #lines(x, y3, col="dark red", lty=3)
+    
+    if(v$query == 1) {
+      # Blockchain stats
+      x <- unlist(v$data["time"], use.names=FALSE)
+      y1 <- unlist(v$data["blocks"], use.names=FALSE)
+      y2 <- unlist(v$data["transactions"], use.names=FALSE)
+      y3 <- unlist(v$data["coins"], use.names=FALSE)
+      df1 <- data.frame(x, y1)
+      df2 <- data.frame(x, y2)
+      df3 <- data.frame(x, y3)
+      ggplot() + 
+        geom_line(data=df1, aes(x, y1, color="Blocks"), size=1, linetype="dashed") + geom_point(data=df1, aes(x, y1, color="Blocks"), size=2, shape=15) + geom_text(aes(x, y1, label=y1, color="Blocks"), hjust=1, vjust=1.8) +
+        geom_line(data=df2, aes(x, y2, color="Transactions"), size=1, linetype="dashed") + geom_point(data=df2, aes(x, y2, color="Transactions"), size=2, shape=16) + geom_text(aes(x, y2, label=y2, color="Transactions"), hjust=1, vjust=-1) +
+        geom_line(data=df3, aes(x, y3, color="Coins"), size=1, linetype="dashed") + geom_point(data=df3, aes(x, y3, color="Coins"), size=2, shape=17) + geom_text(aes(x, y3, label=y3, color="Coins"), hjust=1, vjust=-1) +
+        labs(x = "Tempo", y = "Valori", color="Legenda") +
+        theme_bw() +
+        theme(legend.position="right", legend.title=element_text(size=15), legend.key.size=unit(0.5, "cm"), legend.key.width=unit(2, "cm")) +
+        guides(col = guide_legend(nrow=3,byrow=TRUE), linetype=FALSE)
+    } else if(v$query == 2) {
+      # Block mining time
+      x <- factor(unlist(v$data["block"], use.names=FALSE), levels = unlist(v$data["block"], use.names=FALSE))
+      y <- unlist(v$data["miningtime"], use.names=FALSE)
+      ggplot(v$data, aes(x, y)) + 
+        geom_bar(stat="identity", width=.3, fill="darkred") + 
+        geom_text(aes(x, y, label=y, hjust=0.5, vjust=-1)) +
+        labs(x = "Blocchi", y = "Tempo di mining") +
+        theme_bw() 
+    } else if(v$query == 3) {
+      # Transactions waiting time
+      x <- factor(unlist(v$data["transaction"], use.names=FALSE), levels = unlist(v$data["transaction"], use.names=FALSE))
+      y <- unlist(v$data["waitingtime"], use.names=FALSE)
+      ggplot(v$data, aes(x, y)) + 
+        geom_bar(stat="identity", width=.3, fill="darkblue") + 
+        geom_text(aes(x, y, label=y, hjust=0.5, vjust=-1)) +
+        labs(x = "Transazioni", y = "Tempo di attesa") +
+        theme_bw() 
+    }
   })
   
-  output$transactions <- renderPlot({
-    x <- ts(v$data["time"])
-    y <- ts(v$data["transactions"])
-    plot.ts(x, y, main = v$title, xlab = "Time", ylab = "Value")
-    points(x,y)
-    #legend("top", legend = "Lunghezza della Blockchain", col = "black", lty = 1)
-  })
   
-  output$coins <- renderPlot({
-    x <- ts(v$data["time"])
-    y <- ts(v$data["coins"])
-    plot.ts(x, y, main = v$title, xlab = "Time", ylab = "Value")
-    points(x,y)
-    #legend("top", legend = "Lunghezza della Blockchain", col = "black", lty = 1)
-  })
+  # HTTP REQUEST FUNCTIONS #
+  getStats = function() {
+    #obj <- fromJSON("http://localhost:3001/webresources/blockchainstats")                   # bisogna controllare che obj sia una lista con i 4 elementi delle statistiche!
+    load(file="/home/alessandro/Projects/SkanCoin/DiagnosticClient/app/data/data.Rda")  
+    #rbind(stat, obj)
+    #save(stat,file="/home/alessandro/Projects/SkanCoin/DiagnosticClient/app/data/data.Rda")
+    stat
+  }
   
+  getBlocksMiningTime = function() {
+    #obj <- fromJSON("http://localhost:3001/webresources/blocksminingtime")                   # bisogna controllare cosa restituisce
+    stat <- data.frame("block"=c(1, 2, 3, 4, 5, 6, 7), "miningtime"=c(12, 15, 20, 11, 30, 12, 10))
+  }
+  
+  getWaitingTransactionMiningTime = function() {
+    #obj <- fromJSON("http://localhost:3001/webresources/transminingtime")                    # bisogna controllare cosa restituisce
+    stat <- data.frame("transaction"=c(1, 2, 3, 4, 5, 6, 7), "waitingtime"=c(1, 5, 5, 50, 12, 134, 73))
+  }
 }
 
 # Run the app ----
